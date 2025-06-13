7948a72fdde0ce33ff8ec2744768af6f5f8b6262
Roza Antonevici <rozic.antonevici@me.com>
2025-06-14 00:22:35 +0200

    Create info.java
    Created info package with my uni matriculation number and other relevant information

---
+++ src/info.java
@@ -0,0 +1,13 @@
+package shippingCompany;
+
+public class info {
+	public static void main(String[] args) {
+		System.out.println("Project: Shipping Company");
+		System.out.println("Created by: Roza Antonevici");
+		System.out.println("Matriculation Number: xxxxxxxx");
+		System.out.println("Group B");
+		System.out.println("Date of creation: 13 June 2025");
+		System.out.println("Lines of code:");
+	}
+
+}
