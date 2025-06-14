5e0b474c3a15d3b554ea33d39f7d27ec6ca225fb
Roza Antonevici <rozic.antonevici@me.com>
2025-06-15 01:41:49 +0200

    Update MediumTruck.java
    refactored and commented MediumTruck class in tanker package

--- src/tanker/MediumTruck.java
+++ src/tanker/MediumTruck.java
@@ -1,21 +1,28 @@
 //Roza Antonevici
 package tanker;
-
 public class MediumTruck extends Truck {
-    
-    public MediumTruck() {
-        super(9.65, 1.02, "Medium");
-    }
-    
-    @Override
-    public void printTruckInfo() {
-        System.out.println("Medium Truck Info");
-        System.out.println("Type: " + type);
-        System.out.println("Length: " + String.format("%.2f", length) + " m");
-        System.out.println("Radius: " + String.format("%.2f", radius) + " m");
-        System.out.println("Volume: " + String.format("%.2f", calculateVolume()) + " m³");
-        System.out.println("Capacity: " + String.format("%.2f", getVolumeInGallons()) + " gallons");
-        System.out.println("made by Roza");
-    }
-    
+
+  /**
+   *this is a constructor for MediumTruck
+   *it initializes with:
+   *Length: 380 inches ≈ 9.65 meters
+   *Radius: 40 inches ≈ 1.02 meters
+   *and is calling the superclass (Truck) constructor to set these values
+   */
+  public MediumTruck() {
+    super("Medium", 9.65, 1.02);
+  }
+
+  /**
+   *Overrides the printTruckInfo() method to display
+   *this truck’s dimensions and calculated volume
+   */
+  @Override
+  public void printTruckInfo() {
+    System.out.println("\nMedium Truck Information:");
+    System.out.println("Length: " + String.format("%.2f", getLengthMeters()) + " meters");
+    System.out.println("Radius: " + String.format("%.2f", getRadiusMeters()) + " meters");
+    System.out.println("Volume: " + String.format("%.2f", getVolumeGallons()) + " gallons (" +
+        String.format("%.2f", getVolumeCubicMeters()) + " cubic meters)");
+  }
 }
