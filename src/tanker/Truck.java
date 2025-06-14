58716265f68c11cc766cccabeeafd650c45c883b
Roza Antonevici <rozic.antonevici@me.com>
2025-06-14 14:38:25 +0200

    Update Truck.java
    separating main truck class into subclasses

--- src/tanker/Truck.java
+++ src/tanker/Truck.java
@@ -1,57 +1,52 @@
 //Roza Antonevici
 package tanker;
 
-public class Truck {
-	
-	//private attributes
-		private double length;
-		private double radius;
-		private boolean small;
-	
-	//constructor
-	public Truck (double length, double radius, boolean small) {
-		this.length=length;
-		this.radius=radius;
-		this.small=small; //true if the container is small
-			
-	}
-	//generated getters and setters
-
-	public double getLength() {
-		return length;
-	}
-
-	public void setLength(double length) {
-		this.length = length;
-	}
-
-	public double getRadius() {
-		return radius;
-	}
-	
-	public boolean isSmall() {
-		return small;
-	}
-
-	public void setSmall(boolean small) {
-		this.small = small;
-	}
-
-	//calculates  volume
-	public double getVolume() {
-		double volume;
-		volume= this.length * this.radius;
-		return volume;
-		
-	}
-	
-	//prints truck info
-	public void printTruckInfo() {
-		String str;
-		str= "Truck length: "+ this.length+"m"+"\nRadius: "+ this.radius+"m"+"\nVolume: "+this.getVolume()+"m3"; 
-		System.out.println(str);
-		
-	}
-		
-
+public abstract class Truck {
+    protected double length; //in meters
+    protected double radius; //in meters
+    protected String type;
+    
+    //Constructor
+    public Truck(double length, double radius, String type) {
+        this.length = length;
+        this.radius = radius;
+        this.type = type;
+    }
+    
+    //Getters and Setters
+    public double getLength() { 
+        return length; 
+    }
+    
+    public void setLength(double length) { 
+        this.length = length; 
+    }
+    
+    public double getRadius() { 
+        return radius; 
+    }
+    
+    public void setRadius(double radius) { 
+        this.radius = radius; 
+    }
+    
+    public String getType() { 
+        return type; 
+    }
+    
+    public void setType(String type) { 
+        this.type = type; 
+    }
+    
+    //Calculating volume in cubic meters (π × r² × length)
+    public double calculateVolume() {
+        return Math.PI * Math.pow(radius, 2) * length;
+    }
+    
+    //Converting volume to gallons (1 cubic meter = 264.172 gallons)
+    public double getVolumeInGallons() {
+        return calculateVolume() * 264.172;
+    }
+    
+    public abstract void printTruckInfo();
 }
