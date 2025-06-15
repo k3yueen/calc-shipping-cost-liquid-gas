e3e149d90edd11158e5d7db2b379db592ffae30a
Roza Antonevici <rozic.antonevici@me.com>
2025-06-15 02:01:45 +0200

    Update LiquidItem.java
    optimised refactored and commented lines of code

--- src/tanks/LiquidItem.java
+++ src/tanks/LiquidItem.java
@@ -1,21 +1,25 @@
 //Roza Antonevici
 package tanks;
 
-	public class LiquidItem extends Item {
-	    
-	    public LiquidItem(String name, double riskFactor, double densityKgPerGallon, double quantityGallons) {
-	        super(name, riskFactor, densityKgPerGallon, quantityGallons);
-	    }
-	    
-	    @Override
-	    public void printItemInfo() {
-	        System.out.println("Item Information");
-	        System.out.println("Name: " + getName());
-	        System.out.println("Quantity: " + String.format("%.2f", getQuantityGallons()) + " gallons");
-	        System.out.println("Volume: " + String.format("%.2f", calculateVolume()) + " m³");
-	        System.out.println("Weight: " + String.format("%.2f", calculateWeight()) + " kg");
-	        System.out.println("Risk Factor: " + getRiskFactor() + "%");
-	        System.out.println("Density: " + getDensityKgPerGallon() + " kg/gallon");
-	        System.out.println("made by Roza");
-	    }
+//this class represents a specific type of Item which is a liquid and extends the abstract Item class
+
+//constructor that initializes the liquid item with the given parameters
+public class LiquidItem extends Item {
+  public LiquidItem(String name, String chemicalFormula, double riskFactor, double densityKgPerGallon) {
+    super(name, chemicalFormula, riskFactor, densityKgPerGallon);
+  }
+
+//this method overrides the abstract method from the Item class
+//it  prints detailed information about the liquid item
+  
+  @Override
+  public void printItemInfo() {
+    System.out.println("\nLiquid Item Information:");
+    System.out.println("Name: " + getName());
+    System.out.println("Chemical Formula: " + getChemicalFormula());
+    System.out.println("Risk Factor: " + getRiskFactor() + "%");
+    System.out.println("Density: " + getDensityKgPerGallon() + " kg/gallon");
+    System.out.println("Volume: " + getVolumeGallons() + " gallons (" + getVolumeCubicMeters() + " cubic meters)");
+    System.out.println("Weight: " + calculateWeight() + " kg");
+  }
 }
